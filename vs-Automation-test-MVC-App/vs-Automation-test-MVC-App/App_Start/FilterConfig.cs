628bd5324f25b52f640c89107f80bb5d3dd7adf6
egbill3eagle <bill.egbill3eagle+github@outlook.com>
2016-08-22 00:51:21 -0500

    Initial Commit

---
+++ vs-Automation-test-MVC-App/vs-Automation-test-MVC-App/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace vs_Automation_test_MVC_App
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ vs-Automation-test-MVC-App/vs-Automation-test-MVC-App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vs_Automation_test_MVC_App.MvcApplication" Language="C#" %>
